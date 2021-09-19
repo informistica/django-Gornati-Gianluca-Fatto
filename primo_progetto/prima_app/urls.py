d3439b625e2c9e55bee44a8f42182141ef04134a
CommentatoreVideo <54153405+CommentatoreVideo@users.noreply.github.com>
2021-09-19 14:00:16 +0200

    Livello 4

--- primo_progetto/prima_app/urls.py
+++ primo_progetto/prima_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from prima_app.views import homepage,welcome,lista,chi_siamo
-
+from prima_app.views import homepage,welcome,lista,chi_siamo,variabili
 app_name="prima_app"
 urlpatterns=[
   path('', homepage, name='homepage'),
   path('welcome',welcome,name='welcome'),
   path('lista',lista,name='lista'),
-  path('chi_siamo',chi_siamo,name='chi_siamo')
+  path('chi_siamo',chi_siamo,name='chi_siamo'),
+  path('variabili',variabili,name='variabili'),
 ]
