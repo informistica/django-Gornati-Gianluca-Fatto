dc1410be8a2dca4db7e14df958c1b5fbb0deb966
CommentatoreVideo <54153405+CommentatoreVideo@users.noreply.github.com>
2021-11-23 16:24:07 +0100

    Livello 12

--- news/models.py
+++ news/models.py
@@ -6,8 +6,11 @@ class Giornalista(models.Model):
   nome=models.CharField(max_length=20)
   cognome=models.CharField(max_length=20)
   
-  def __str__(self):
-    return self.nome+" "+self.cognome
+  # def __str__(self):
+  #   return self.nome+" "+self.cognome
+  class Meta:
+    verbose_name="Giornalista"
+    verbose_name_plural="Giornalisti"
 
 class Articolo(models.Model):
   """Il modello generico di un articolo di news"""
@@ -15,5 +18,9 @@ class Articolo(models.Model):
   contenuto=models.TextField()
   giornalista=models.ForeignKey(Giornalista, on_delete=models.CASCADE, related_name="articoli")
   
-  def __str__(self):
-    return self.titolo
+  # def __str__(self):
+  #   return self.titolo
+    
+  class Meta:
+    verbose_name="Articolo"
+    verbose_name_plural="Articoli"
