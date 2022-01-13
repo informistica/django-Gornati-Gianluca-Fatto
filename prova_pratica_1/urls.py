5de1c5106dd975de7eb4a590e5d9d26d6f7540cb
CommentatoreVideo <54153405+CommentatoreVideo@users.noreply.github.com>
2022-01-13 10:34:00 +0100

    Aggiunto prova pratica 1

---
+++ prova_pratica_1/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import view_b, view_c
+app_name="prova_pratica_1"
+urlpatterns=[
+  path('view_b',view_b, name='view_b'),
+  path('view_c',view_c,name='view_c'),
+]
